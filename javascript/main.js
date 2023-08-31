65c2db62c03b38b16e40efe6c457c36fe871fb2c
Hiba Ghrab <112657523+hibatoarrahmen@users.noreply.github.com>
2023-08-31 17:02:21 +0200

    version 1

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+
+    const inputBox = document.getElementById("input-box");
+    const listContainer = document.getElementById("list-container");
+    function addTask() {
+        if (inputBox === "") {
+            alert("you must write something ");
+        } else {
+            let li = document.createElement("li");
+        }
+    }
+
